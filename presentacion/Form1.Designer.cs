16578984501f66411c0ebdd50023ea2199622504
nicodiaz00 <ngdiaz00@gmail.com>
2024-01-22 18:57:43 -0300

    agregando busqueda avanzada

--- presentacion/Form1.Designer.cs
+++ presentacion/Form1.Designer.cs
@@ -33,9 +33,20 @@
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            this.txtFiltroBusqueda = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBusquedaAvanzada = new System.Windows.Forms.Button();
+            this.txtAvanzado = new System.Windows.Forms.TextBox();
+            this.lblFiltroAvanzado = new System.Windows.Forms.Label();
+            this.cboCriterio = new System.Windows.Forms.ComboBox();
+            this.lblCriterio = new System.Windows.Forms.Label();
+            this.cboCampo = new System.Windows.Forms.ComboBox();
+            this.lblCampo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxArticulo)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvArticulos
@@ -47,7 +58,7 @@
             this.dgvArticulos.Name = "dgvArticulos";
             this.dgvArticulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvArticulos.Size = new System.Drawing.Size(643, 248);
-            this.dgvArticulos.TabIndex = 0;
+            this.dgvArticulos.TabIndex = 5;
             this.dgvArticulos.SelectionChanged += new System.EventHandler(this.dgvArticulos_SelectionChanged);
             // 
             // pictureBoxArticulo
@@ -92,21 +103,121 @@
             this.btnEliminar.UseVisualStyleBackColor = false;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
-            // button1
+            // btnBuscar
             // 
-            this.button1.Location = new System.Drawing.Point(352, 336);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 38);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnBuscar.Location = new System.Drawing.Point(239, 39);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(59, 21);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Location = new System.Drawing.Point(9, 43);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(32, 13);
+            this.lblFiltro.TabIndex = 6;
+            this.lblFiltro.Text = "Filtro:";
+            // 
+            // txtFiltroBusqueda
+            // 
+            this.txtFiltroBusqueda.Location = new System.Drawing.Point(51, 40);
+            this.txtFiltroBusqueda.Name = "txtFiltroBusqueda";
+            this.txtFiltroBusqueda.Size = new System.Drawing.Size(182, 20);
+            this.txtFiltroBusqueda.TabIndex = 0;
+            this.txtFiltroBusqueda.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFiltroBusqueda_KeyPress);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnBusquedaAvanzada);
+            this.groupBox1.Controls.Add(this.txtAvanzado);
+            this.groupBox1.Controls.Add(this.lblFiltroAvanzado);
+            this.groupBox1.Controls.Add(this.cboCriterio);
+            this.groupBox1.Controls.Add(this.lblCriterio);
+            this.groupBox1.Controls.Add(this.cboCampo);
+            this.groupBox1.Controls.Add(this.lblCampo);
+            this.groupBox1.Location = new System.Drawing.Point(386, 336);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(568, 64);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Busqueda avanzada";
+            // 
+            // btnBusquedaAvanzada
+            // 
+            this.btnBusquedaAvanzada.Location = new System.Drawing.Point(443, 31);
+            this.btnBusquedaAvanzada.Name = "btnBusquedaAvanzada";
+            this.btnBusquedaAvanzada.Size = new System.Drawing.Size(95, 22);
+            this.btnBusquedaAvanzada.TabIndex = 6;
+            this.btnBusquedaAvanzada.Text = "Buscar";
+            this.btnBusquedaAvanzada.UseVisualStyleBackColor = true;
+            this.btnBusquedaAvanzada.Click += new System.EventHandler(this.btnBusquedaAvanzada_Click);
+            // 
+            // txtAvanzado
+            // 
+            this.txtAvanzado.Location = new System.Drawing.Point(339, 32);
+            this.txtAvanzado.Name = "txtAvanzado";
+            this.txtAvanzado.Size = new System.Drawing.Size(76, 20);
+            this.txtAvanzado.TabIndex = 5;
+            // 
+            // lblFiltroAvanzado
+            // 
+            this.lblFiltroAvanzado.AutoSize = true;
+            this.lblFiltroAvanzado.Location = new System.Drawing.Point(301, 36);
+            this.lblFiltroAvanzado.Name = "lblFiltroAvanzado";
+            this.lblFiltroAvanzado.Size = new System.Drawing.Size(32, 13);
+            this.lblFiltroAvanzado.TabIndex = 4;
+            this.lblFiltroAvanzado.Text = "Filtro:";
+            // 
+            // cboCriterio
+            // 
+            this.cboCriterio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCriterio.FormattingEnabled = true;
+            this.cboCriterio.Location = new System.Drawing.Point(210, 33);
+            this.cboCriterio.Name = "cboCriterio";
+            this.cboCriterio.Size = new System.Drawing.Size(85, 21);
+            this.cboCriterio.TabIndex = 3;
+            // 
+            // lblCriterio
+            // 
+            this.lblCriterio.AutoSize = true;
+            this.lblCriterio.Location = new System.Drawing.Point(162, 36);
+            this.lblCriterio.Name = "lblCriterio";
+            this.lblCriterio.Size = new System.Drawing.Size(42, 13);
+            this.lblCriterio.TabIndex = 2;
+            this.lblCriterio.Text = "Criterio:";
+            // 
+            // cboCampo
+            // 
+            this.cboCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCampo.FormattingEnabled = true;
+            this.cboCampo.Location = new System.Drawing.Point(56, 33);
+            this.cboCampo.Name = "cboCampo";
+            this.cboCampo.Size = new System.Drawing.Size(91, 21);
+            this.cboCampo.TabIndex = 1;
+            this.cboCampo.SelectedIndexChanged += new System.EventHandler(this.cboCampo_SelectedIndexChanged);
+            // 
+            // lblCampo
+            // 
+            this.lblCampo.AutoSize = true;
+            this.lblCampo.Location = new System.Drawing.Point(7, 36);
+            this.lblCampo.Name = "lblCampo";
+            this.lblCampo.Size = new System.Drawing.Size(43, 13);
+            this.lblCampo.TabIndex = 0;
+            this.lblCampo.Text = "Campo:";
             // 
             // frmVentanaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(983, 436);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(983, 541);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtFiltroBusqueda);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnModificar);
             this.Controls.Add(this.btnAgregar);
@@ -117,7 +228,10 @@
             this.Load += new System.EventHandler(this.frmVentanaPrincipal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxArticulo)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -128,7 +242,17 @@
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnModificar;
         private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label lblFiltro;
+        private System.Windows.Forms.TextBox txtFiltroBusqueda;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cboCriterio;
+        private System.Windows.Forms.Label lblCriterio;
+        private System.Windows.Forms.ComboBox cboCampo;
+        private System.Windows.Forms.Label lblCampo;
+        private System.Windows.Forms.Button btnBusquedaAvanzada;
+        private System.Windows.Forms.TextBox txtAvanzado;
+        private System.Windows.Forms.Label lblFiltroAvanzado;
     }
 }
 
--- presentacion/Form1.cs
+++ presentacion/Form1.cs
@@ -20,6 +20,29 @@ namespace presentacion
         {
             InitializeComponent();
         }
+        public void ocultarColumnas()
+        {
+            dgvArticulos.Columns["UrlImagen"].Visible = false;
+            dgvArticulos.Columns["Id"].Visible = false; //oculto columna id
+        }
+        public void busquedaArticulo()
+        {
+            List<Articulo> listaArticuloFiltro;
+            string filtro = txtFiltroBusqueda.Text;
+            if (filtro.Length <= 2)
+            {
+                listaArticuloFiltro = listaArticulo.FindAll(art => art.Nombre.ToLower().Contains(filtro.ToLower()) || art.Descripcion.ToLower().Contains(filtro.ToLower()));
+            }
+            else
+            {
+                listaArticuloFiltro = listaArticulo;
+            }
+
+
+            dgvArticulos.DataSource = null;
+            dgvArticulos.DataSource = listaArticuloFiltro;
+            ocultarColumnas();
+        }
 
         private void cargarImagen(string imagen)
         {
@@ -36,13 +59,20 @@ namespace presentacion
         private void frmVentanaPrincipal_Load(object sender, EventArgs e)
         {
             cargarLista();
+            cboCampo.Items.Add("Codigo");
+            cboCampo.Items.Add("Nombre");
+            cboCampo.Items.Add("Precio");
         }
 
 
         private void dgvArticulos_SelectionChanged(object sender, EventArgs e)
         {
-            Articulo seleccionado = (Articulo)dgvArticulos.CurrentRow.DataBoundItem;
-            cargarImagen(seleccionado.UrlImagen);
+            if(dgvArticulos.CurrentRow != null)
+            {
+                Articulo seleccionado = (Articulo)dgvArticulos.CurrentRow.DataBoundItem;
+                cargarImagen(seleccionado.UrlImagen);
+            }
+            
             
         }
 
@@ -62,8 +92,7 @@ namespace presentacion
 
                 listaArticulo = negocio.listarArticulo();
                 dgvArticulos.DataSource = listaArticulo;
-                dgvArticulos.Columns["UrlImagen"].Visible = false;
-                dgvArticulos.Columns["Id"].Visible = false; //oculto columna id
+                ocultarColumnas();
                 cargarImagen(listaArticulo[0].UrlImagen);
             }
             catch (Exception ex)
@@ -103,6 +132,62 @@ namespace presentacion
 
                 MessageBox.Show(ex.ToString());
             }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            busquedaArticulo();
+        }
+
+        private void txtFiltroBusqueda_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            busquedaArticulo();
+        }
+
+        private void cboCampo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string seleccionado = cboCampo.SelectedItem.ToString();
+            if(seleccionado == "Codigo")
+            {
+                cboCriterio.Items.Clear();
+                cboCriterio.Items.Add("Contiene");
+                cboCriterio.Items.Add("Empieza con");
+                cboCriterio.Items.Add("Termina con");
+
+
+            }else if(seleccionado == "Nombre")
+            {
+                cboCriterio.Items.Clear();
+                cboCriterio.Items.Add("Contiene");
+                cboCriterio.Items.Add("Empieza");
+                cboCriterio.Items.Add("Termina");
+            }
+            else
+            {
+                cboCriterio.Items.Clear();
+                cboCriterio.Items.Add("Mayor a");
+                cboCriterio.Items.Add("Menor a");
+                cboCriterio.Items.Add("Igual a");
+            }
+        }
+
+        private void btnBusquedaAvanzada_Click(object sender, EventArgs e)
+        {
+            ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+            try
+            {
+                string campo = cboCampo.SelectedItem.ToString();
+                string criterio = cboCriterio.SelectedItem.ToString();
+                string filtroBusqueda = txtAvanzado.Text;
+
+                dgvArticulos.DataSource = articuloNegocio.filtrarArticulo(campo, criterio, filtroBusqueda);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
         }
     }
 }
